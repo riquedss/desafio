edfec1bfa93b41174b9f7917724923f5eafa5524
Henrique Santana <hsatana.sousass@gmail.com>
2023-12-03 23:01:59 -0300

    métodos para salvar no banco e buscar

--- app/model/musica.rb
+++ app/model/musica.rb
@@ -1,9 +1,20 @@
-class Musica
-  attr_accessor :nome, :letra, :nome_autor
+require '../../db/db.rb'
 
-  def initialize(nome, letra, nome_autor)
+class Musica
+  attr_accessor :id, :nome, :letra, :nome_autor
+
+  def initialize(id, nome, letra, nome_autor)
+    @id = id
     @nome = nome
     @letra = letra
     @nome_autor = nome_autor
+  end
+
+  def save
+    Db.new.escrever(@id, { id: @id, nome: @nome, letra: @letra, nome_autor: @nome_autor })
+  end
+
+  def self.find(id)
+    Db.new.find(id)
   end
 end
